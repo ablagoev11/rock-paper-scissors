9f1f9551a84a3d94a0f0cc5c202f766d4aef5833
ablagoev11 <a.blagoev52@gmail.com>
2024-02-12 01:37:35 +0200

    Add computerChoice that is random every time called

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+const playersChoice = prompt("What is you choice: Rock, Paper, Scissors");
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  if (randomNumber === 1) return "Rock";
+  else if (randomNumber === 2) return "Paper";
+  else return "Scissors";
+}
