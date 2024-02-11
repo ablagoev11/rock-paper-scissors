2f0b68d853724cbbf008932e4d563d63f0b672fb
ablagoev11 <a.blagoev52@gmail.com>
2024-02-12 01:55:29 +0200

    Add case sensitivity processor to player's prompt

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const playersPrompt = prompt("What is you choice: Rock, Paper, Scissors");
+const playersChoice = processCaseSensitivity(playersPrompt);
 const computersChoice = getComputerChoice();
-alert(playRound(playersPrompt, computersChoice));
+alert(playRound(playersChoice, computersChoice));
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -24,4 +25,11 @@ function playRound(playersChoice, computersChoice) {
     else if (computersChoice === "Paper") return "You WIN!";
     else return "DRAW";
   }
+}
+
+function processCaseSensitivity(playersChoice) {
+  return (
+    playersChoice.charAt(0).toUpperCase() +
+    playersChoice.slice(1, playersChoice.length).toLowerCase()
+  );
 }
